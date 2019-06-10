0d4d6aa464d43ba31ad24238bb083f938d24fbea
VladKhram <vkhram4enko@gmail.com>
2019-06-10 11:53:59 +0300

    transferred functions from addRemovePrintLine.js to class Canv

--- classCanvas/classCanvas.js
+++ classCanvas/classCanvas.js
@@ -1,39 +1,149 @@
 class Canv{
 
+
 	constructor(width, height){
 		let element = this.element = document.createElement('canvas');
 		this.setSize(width, height);
-		addPrintLine(element);
+		this.isDown = false;
+		this.addPrintLine();
+
 	}
 
+
+	//Функция рисования линии
+	printLine(e){
+		
+		let canvas = e.target,
+			context = canvas.getContext('2d');
+
+		//Функция получения координат точек ломаной
+		function getCoord(e){
+			let newX = e.clientX - canvasPosition.left,
+				newY = e.clientY - canvasPosition.top;
+			coords.push([newX, newY]);
+		}
+
+		//Функция рисования ломаной
+		let animation = () => {
+
+			let len = coords.length;
+
+			//Проверка на наличие как минимум 2ух точек
+			if (len > 1){
+
+				let x, y;
+
+				//Начало рисования
+				context.beginPath();
+				context.moveTo(coords[0][0],coords[0][1]);
+
+				//Рисование отрезков
+				for (let i = 1; i < len; i++){
+
+					x = coords[i][0];
+					y = coords[i][1];
+					context.lineTo(x, y);
+
+				}
+
+				context.stroke();
+
+				//Связывание конца данной ломаной с началом следующей
+				coords = [[x, y]];
+
+			}
+
+			//Проверка нажатия на канвас
+			if(this.isDown){
+				requestAnimationFrame(animation);
+			} else{
+				canvas.removeEventListener('mousemove', getCoord);
+			}
+
+		}
+
+
+		//Инициализация первой точки линии
+		let canvasPosition 	= canvas.getBoundingClientRect(), 
+			firstX = e.clientX - canvasPosition.left,
+		 	firstY = e.clientY - canvasPosition.top,
+		 	coords = [[firstX, firstY]];
+
+		//Получение координат новой точки
+		canvas.addEventListener('mousemove', getCoord);
+	 	
+		//Начало рисования линии
+		this.isDown = true;
+		requestAnimationFrame(animation);
+	}
+
+
+	//Функция прекращения удерживания ЛКМ
+	falseIsDown(e){
+
+		this.isDown = false;
+
+	}
+
+
+	//Функция рисования линии на канвасе при нажатой ЛКМ
 	addPrintLine(){
 
-		removePrintLine(this.element);
-		addPrintLine(this.element);
+		let canvas = this.element;
+
+		if(canvas.getContext){
+
+			//События вызывающие начало/прекращение рисования линии
+			canvas.addEventListener('mousedown', this.printLine);
+
+			canvas.addEventListener('mouseup', this.falseIsDown);
+			canvas.addEventListener('mouseleave', this.falseIsDown);
+
+		}
 
 	}
 
+
+	//Функция отмены рисования линии на канвасе при нажатой ЛКМ
 	removePrintLine(){
 
-		removePrintLine(this.element);
+		let canvas = this.element;
+
+		//Очистка событий вызывающих начало/прекращение рисования линии
+		canvas.removeEventListener('mousedown', this.printLine);
+
+		canvas.removeEventListener('mouseup', this.falseIsDown);
+		canvas.removeEventListener('mouseleave', this.falseIsDown);
 
 	}
 
+
+	//Функция задания размера канваса
 	setSize(width, height){
 		this.element.width = +width;
 		this.element.height = +height;
 	}
 
-	setWidth(width){
+
+	//Функция задания ширины канваса
+	set width(width){
 		this.element.width = +width;
 	}
 
-	setHeight(height){
+
+	//Функция задания высоты канваса
+	set height(height){
+
 		this.element.height = +height;
+
 	}
 
-	getCanvas(element) {
+
+	//Функция возвращающая канвас
+	get canvas() {
+
 		return this.element;
+
 	}
 
 }
--- classCanvas/script.js
+++ classCanvas/script.js
@@ -1,13 +1,13 @@
 //Подключение к канвасу функции рисования
 let canvas = new Canv(),
-	canvas2 = new Canv(400, 400);
+	canvas2 = new Canv();
 
 canvas.setSize(500, 500);
+canvas2.setSize(300, 300);
 canvas2.removePrintLine();
 
-document.body.appendChild(canvas.getCanvas());
-document.body.appendChild(canvas2.getCanvas());
+document.body.appendChild(canvas.canvas);
+document.body.appendChild(canvas2.canvas);
 
-canvas2.addPrintLine();
 
 
