86edb54170c09839ac9b7a7161f18de2861955a0
Uladzislau Khramchanka <UKhramchanka@gomel.iba.by>
2019-06-18 21:29:19 +0300

    Rebuild functions in classCanvas and add tools (straight, circle, rect)

--- classCanvas/classCanvas.js
+++ classCanvas/classCanvas.js
@@ -1,61 +1,280 @@
 class Canv{
 
-
 	constructor(width, height){
 		let element = this.element = document.createElement('canvas');
 		this.setSize(width, height);
-
-		//Получение индекса канваса
-		this.n = Canv.canvasId();
-		element.id = "canvas-" + this.n;
 
 		//Создание кэша для нарисованных/удалённых линий
 		element.cash = [];
 		element.removed = [];
 
-		this.addPrintLine();
+		this.addPrint('Line');
 
 	}
 
-	//Функция получения ндекса канваса
-	static canvasId(){
 
-		if (Canv.n == undefined){
+	//Функция для полученние координат мышки
+	static getMouseCoord(e){
 
-			Canv.n = 0;
+		let canvas = e.target,
+			canvasPosition 	= canvas.getBoundingClientRect(), 
+			newX = e.clientX - canvasPosition.left,
+		 	newY = e.clientY - canvasPosition.top;
 
-		} else{
- 
-			Canv.n++;
+		canvas.mouseCoord = [newX, newY];
 
+	}
+
+
+	//Функция для рисования линии
+	static line(context, coords){
+
+		let len = coords.length;
+
+		//Начало рисования
+		context.beginPath();
+		context.moveTo(coords[0][0],coords[0][1]);
+
+		//Рисование отрезков
+		for (let i = 1; i < len; i++){
+			context.lineTo(coords[i][0], coords[i][1]);
 		}
 
-		return Canv.n;
+		context.stroke();
 
 	}
 
 
-	//Функция рисования линии
-	printLine(e){
+	//Функция для рисования круга
+	static circle(context, coords){
 
-		let canvas 	= this,
-			context = canvas.getContext('2d');
+		let x 		= coords[0],
+			y 		= coords[1],
+			radius 	= coords[2];
 
-		//Обнуление локальной линии и кэша удалённых линий
-		canvas.path = [];
+		context.beginPath();
+		context.arc(x, y, radius, 0, 2 * Math.PI, false); 
+		context.stroke();
+
+	}
+
+
+	//Функция для рисования прямоугольника
+	static rect(context, coords){
+
+		let x1 = coords[0],
+			y1 = coords[1],
+			x2 = coords[2],
+			y2 = coords[3];
+
+		context.strokeRect(x1, y1, x2, y2);
+
+	}
+
+
+	//Функция для рисование на канвасе из кэша
+	static printCash(canvas){
+
+		let context = canvas.getContext('2d'),
+			cash 	= canvas.cash,
+			len 	= cash.length;
+
+		//Очистка канваса
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		//Перерисовывание элементов канваса без последней линии
+		for (let i = 0; i < len; i++){
+			Canv[cash[i][0]](context, cash[i][1]);
+		}
+
+	}
+
+
+	//Функция для подготовки к рисованию
+	static startPrint(canvas){
+
+		//Обнуление кэша удалённых элементов канваса
 		canvas.removed = [];
 
-		//Функция получения координат точек ломаной
-		function getCoord(e){
+		//Получение координат новой точки
+		canvas.addEventListener('mousemove', Canv.getMouseCoord);
+	 	
+		//Начало рисования линии
+		canvas.isDown = true;
 
-			let newX = e.clientX - canvasPosition.left,
-				newY = e.clientY - canvasPosition.top;
+	}
 
-			coords.push([newX, newY]);
+
+	//Функция для отмены последнего шага
+	stepBack(){
+
+		let canvas 	= this.element,
+			context = canvas.getContext('2d'),
+			len 	= canvas.cash.length;
+
+		//Проверка наличия кэша
+		if(len > 0){
+
+			//Добавление последнй линии в кэш удалённых
+			canvas.removed.unshift(canvas.cash.pop());
+			Canv.printCash(canvas);
 
 		}
 
-		//Функция рисования ломаной
+	}
+
+
+	//Функция для возвращения последнего шага
+	returnStep(){
+
+		let canvas = this.element,
+			context = canvas.getContext('2d'),
+			len 	= canvas.removed.length;
+
+		//Проверка наличия отменённых линий
+		if(len > 0){
+
+			//Возвращение последней удалённой линии в кэш
+			canvas.cash.push(canvas.removed.shift());
+			Canv.printCash(canvas);			
+
+		}
+
+	}
+
+
+	//Функция для рисования прямой
+	printStraight(e){
+
+		//Анимация
+		let animation = () => {
+
+			coords[1] = [canvas.mouseCoord[0], canvas.mouseCoord[1]];
+
+			//Рисование прямой
+			Canv.printCash(canvas);
+			Canv.line(context, coords);
+
+			//Проверка нажатия на канвас
+			if(canvas.isDown){
+				requestAnimationFrame(animation);
+			} else{
+				//Занесение прямой в кэш и очистка события получения координат курсора
+				canvas.cash.push(['line', coords]);
+				canvas.removeEventListener('mousemove', Canv.getMouseCoord);
+			}
+
+		}
+
+		let canvas 	= this,
+			context = canvas.getContext('2d'),
+		//Инициализация первой точки линии
+			canvasPosition 	= canvas.getBoundingClientRect(), 
+		 	coords = [[e.clientX - canvasPosition.left, e.clientY - canvasPosition.top]];
+
+		//Обновление координат мышки
+		canvas.mouseCoord = coords[0];
+
+		//Подготовка к рисованию
+		Canv.startPrint(canvas);
+		requestAnimationFrame(animation);
+
+	}
+
+
+	//Функция для рисования круга
+	printCircle(e){
+
+		//Анимация
+		let animation = () => {
+
+			let width = (canvas.mouseCoord[0] - x),
+				height = (canvas.mouseCoord[1] - y),
+				radius = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+
+			context.clearRect(0, 0, canvas.width, canvas.height);
+
+			Canv.printCash(canvas);
+			Canv.circle(context, [x, y, radius]);
+
+			//Проверка нажатия на канвас
+			if(canvas.isDown){
+				requestAnimationFrame(animation);
+			} else{
+				//Занесение круга в кэш и очистка события получения координат курсора
+				canvas.cash.push([ 'circle', [x, y, radius] ]);
+				canvas.removeEventListener('mousemove', Canv.getMouseCoord);
+			}
+
+		}
+
+		let canvas 	= this,
+			context = canvas.getContext('2d'),
+		//Инициализация первой точки линии
+			canvasPosition 	= canvas.getBoundingClientRect(), 	
+			x = e.clientX - canvasPosition.left,
+		 	y = e.clientY - canvasPosition.top;
+
+		//Обновление координат мышки
+		canvas.mouseCoord = [x, y];
+
+		//Подготовка к рисованию
+		Canv.startPrint(canvas);
+		requestAnimationFrame(animation);
+
+	}
+
+
+	//Функция для рисования прямоугольника
+	printRect(e){
+
+		//Анимация
+		let animation = () => {
+
+			let x2 = canvas.mouseCoord[0] - x1,
+				y2 = canvas.mouseCoord[1] - y1;
+
+			//Рисование прямоугольника
+			Canv.printCash(canvas);
+			context.strokeRect(x1, y1, x2, y2);
+
+			//Проверка нажатия на канвас
+			if(canvas.isDown){
+				requestAnimationFrame(animation);
+			} else{
+				//Занесение прямоугольника в кэш и очистка события получения координат курсора
+				canvas.cash.push([ 'rect', [x1, y1, x2, y2] ]);
+				canvas.removeEventListener('mousemove', Canv.getMouseCoord);
+			}
+
+		}
+
+		let canvas 	= this,
+			context = canvas.getContext('2d'),
+		//Инициализация первой точки линии
+			canvasPosition = canvas.getBoundingClientRect(), 
+			x1 = e.clientX - canvasPosition.left,
+		 	y1 = e.clientY - canvasPosition.top;
+
+		//Обновление координат мышки
+		canvas.mouseCoord = [x1, y1];
+
+		//Подготовка к рисованию
+		Canv.startPrint(canvas);
+		requestAnimationFrame(animation);
+
+	}
+
+
+	//Функция для рисования линии
+	printLine(e){
+
+		//Функция для получения координат точек ломаной
+		function getCoord(e){
+			coords.push([e.clientX - canvasPosition.left, e.clientY - canvasPosition.top]);
+		}
+
+		//Анимация рисования ломаной
 		let animation = () => {
 
 			let len = coords.length;
@@ -63,32 +282,18 @@ class Canv{
 			//Проверка на наличие как минимум 2ух точек
 			if (len > 1){
 
-				let x, y;
-
-				//Начало рисования
-				context.beginPath();
-				context.moveTo(coords[0][0],coords[0][1]);
-
-				//Рисование отрезков
-				for (let i = 1; i < len; i++){
-
-					x = coords[i][0];
-					y = coords[i][1];
-					context.lineTo(x, y);
-
-				}
-
-				context.stroke();
+				//Рисование ломаной
+				Canv.line(context, coords);
 
 				//Сохранение локального пути
 				for (let i = 0; i < len; i++){
-
-					canvas.path.push(coords[i]);
-
+					path.push(coords[i]);
 				}
 
 				//Связывание конца данной ломаной с началом следующей
-				coords = [[x, y]];
+				coords = [ 
+							[ coords[len - 1][0], coords[len - 1][1] ] 
+						];
 
 			}
 
@@ -96,19 +301,23 @@ class Canv{
 			if(canvas.isDown){
 				requestAnimationFrame(animation);
 			} else{
-				//Занесение локального пути в кэш
-				canvas.cash.push(canvas.path);
+				//Занесение локального пути в кэш и очистка события получения координат курсора
+				canvas.cash.push(['line', path]);
 				canvas.removeEventListener('mousemove', getCoord);
 			}
 
 		}
 
-
+		let canvas 	= this,
+			context = canvas.getContext('2d'),
+		//Создание локального пути
+			path = [],
 		//Инициализация первой точки линии
-		let canvasPosition 	= canvas.getBoundingClientRect(), 
-			firstX = e.clientX - canvasPosition.left,
-		 	firstY = e.clientY - canvasPosition.top,
-		 	coords = [[firstX, firstY]];
+			canvasPosition = canvas.getBoundingClientRect(), 
+		 	coords = [[e.clientX - canvasPosition.left, e.clientY - canvasPosition.top]];
+
+		//Обнуление кэша удалённых элементов канваса
+		canvas.removed = [];
 
 		//Получение координат новой точки
 		canvas.addEventListener('mousemove', getCoord);
@@ -119,7 +328,7 @@ class Canv{
 	}
 
 
-	//Функция прекращения удерживания ЛКМ
+	//Функция для прекращения удерживания ЛКМ
 	falseIsDown(e){
 
 		this.isDown = false;
@@ -127,17 +336,18 @@ class Canv{
 	}
 
 
-	//Функция рисования линии на канвасе при нажатой ЛКМ
-	addPrintLine(){
+	//Функция для рисования заданным инструментом
+	addPrint(tool){
 
-		this.removePrintLine();
-		
-		let canvas = this.element;
+		this.removePrintAll();
+
+		let canvas 	= this.element,
+			func 	= "print" + tool;
 
 		if(canvas.getContext){
 
-			//События вызывающие начало/прекращение рисования линии
-			canvas.addEventListener('mousedown', this.printLine);
+			//События вызывающие начало/прекращение рисования заданного элемента
+			canvas.addEventListener('mousedown', this[func]);
 
 			canvas.addEventListener('mouseup', this.falseIsDown);
 			canvas.addEventListener('mouseleave', this.falseIsDown);
@@ -147,13 +357,14 @@ class Canv{
 	}
 
 
-	//Функция отмены рисования линии на канвасе при нажатой ЛКМ
-	removePrintLine(){
+	//Функция для отмены рисования заданным инструментом
+	removePrint(tool){
 
-		let canvas = this.element;
+		let canvas 	= this.element,
+			func 	= "print" + tool;
 
-		//Очистка событий вызывающих начало/прекращение рисования линии
-		canvas.removeEventListener('mousedown', this.printLine);
+		//Очистка событий вызывающих начало/прекращение рисования заданным инструментом
+		canvas.removeEventListener('mousedown', this[func]);
 
 		canvas.removeEventListener('mouseup', this.falseIsDown);
 		canvas.removeEventListener('mouseleave', this.falseIsDown);
@@ -161,92 +372,37 @@ class Canv{
 	}
 
 
-	//Функция отмены последнего шага
-	stepBack(){
+	//Функция для отмены рисования
+	removePrintAll(){
 
-		let canvas 	= document.getElementById(this.name),
-			context = canvas.getContext('2d'),
-			len 	= canvas.cash.length;
+		let canvas 	= this.element;
 
-		//Проверка наличия кэша
-		if(len > 0){
+		//Очистка событий вызывающих начало/прекращение рисования
+		canvas.removeEventListener('mousedown', this.printLine);
+		canvas.removeEventListener('mousedown', this.printStraight);
+		canvas.removeEventListener('mousedown', this.printCircle);
+		canvas.removeEventListener('mousedown', this.printRect);
 
-			//Очистка канваса
-			context.clearRect(0, 0, canvas.width, canvas.height);
-
-			//Перерисовывание элементов канваса без последней линии
-			for (let i = 0; i < len - 1; i++){
-
-				let cash = canvas.cash[i],
-					len  = cash.length;
-
-				context.beginPath();
-				context.moveTo(cash[0][0], cash[0][1]);
-
-				for (let j = 1; j < len; j++){
-
-					context.lineTo(cash[j][0], cash[j][1]);
-
-				}
-
-				context.stroke();
-
-			}
-
-			//Добавление последнй линии в кэш удалённых
-			canvas.removed.unshift(canvas.cash.pop());
-
-		}
+		canvas.removeEventListener('mouseup', this.falseIsDown);
+		canvas.removeEventListener('mouseleave', this.falseIsDown);
 
 	}
 
 
-	//Функция возвращения последнего шага
-	returnStep(){
-
-		let canvas 	= document.getElementById(this.name),
-			context = canvas.getContext('2d'),
-			len 	= canvas.removed.length;
-
-		//Проверка наличия отменённых линий
-		if(len > 0){
-
-			let last = canvas.removed.shift(),
-				len  = last.length;
-
-			//Возвращение последней удалённой линии
-			context.beginPath();
-			context.moveTo(last[0][0], last[0][1]);
-
-			for (let i = 1; i < len; i++){
-
-				context.lineTo(last[i][0], last[i][1]);
-
-			}
-
-			context.stroke();
-
-			//Добавление возвращённой линии в кэш
-			canvas.cash.push(last);
-
-		}
-
-	}
-
-	//Функция задания размера канваса
+	//Функция для задания размера канваса
 	setSize(width, height){
 		this.element.width = +width;
 		this.element.height = +height;
 	}
 
 
-	//Функция задания ширины канваса
+	//Функция для задания ширины канваса
 	set width(width){
 		this.element.width = +width;
 	}
 
 
-	//Функция задания высоты канваса
+	//Функция для задания высоты канваса
 	set height(height){
 
 		this.element.height = +height;
@@ -254,29 +410,12 @@ class Canv{
 	}
 
 
-	//Функция возвращающая канвас
+	//Функция для возвращающая канвас
 	get canvas() {
 
-		let btn1 = document.createElement('button'),
-			btn2 = document.createElement('button'),
-			frag = document.createDocumentFragment();
-
-		//Добавление кнопки отмены последнего шага
-		btn1.innerHTML = "Отмена";
-		btn1.name = 'canvas-' + this.n;
-		btn1.onclick = this.stepBack;
-
-		//Добавление кнопки возвращения отменённых шагов
-		btn2.innerHTML = "Вернуть";
-		btn2.name = 'canvas-' + this.n;
-		btn2.onclick = this.returnStep;
-
-		frag.appendChild(this.element);
-		frag.appendChild(btn1);
-		frag.appendChild(btn2);
-
-		return frag;
+		return this.element;
 
 	}
 
 }
+
--- classCanvas/script.js
+++ classCanvas/script.js
@@ -4,7 +4,19 @@ let canvas = new Canv(),
 
 canvas.setSize(500, 500);
 canvas2.setSize(300, 300);
-canvas2.removePrintLine();
 
 document.body.appendChild(canvas.canvas);
+
+let btns = html(`
+	<button onclick="canvas.stepBack()">Отмена</button>
+	<button onclick="canvas.returnStep()">Вернуть</button>
+	<button onclick="canvas.addPrint('Line')">Карандаш</button>
+	<button onclick="canvas.addPrint('Straight')">Прямая</button>
+	<button onclick="canvas.addPrint('Circle')">Окружность</button>
+	<button onclick="canvas.addPrint('Rect')">Прямоугольник</button>`);
+
+
+
 document.body.appendChild(canvas2.canvas);
+document.body.appendChild(btns);
+
